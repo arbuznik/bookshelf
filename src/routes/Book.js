441f4641549c2ec969a9c6b3c9e0b9f5c02dfd62
Arbuznik <arbyzov.nikita@yandex.ru>
2022-01-18 01:18:21 +0300

    [feat] add book reviews

--- src/routes/Book.js
+++ src/routes/Book.js
@@ -17,6 +17,8 @@ function Book() {
       authors,
       categories,
       description,
+      averageRating,
+      ratingsCount,
       imageLinks: {
         large,
         thumbnail,
@@ -45,7 +47,8 @@ function Book() {
         <div className="book__content">
           {authors && <ul className="book__authors">{authors.map((author, index) =>
             <p key={index} className="book__author">{author}</p>)}</ul>}
-          {description && <p dangerouslySetInnerHTML={{__html: description}} className="book__description"></p>}
+          {description && <p dangerouslySetInnerHTML={{__html: description}} className="book__description"/>}
+          {averageRating && <p className="book__reviews">{Array(averageRating).fill(<span className="book__star">⭐</span>).map((val) => val)} based on {ratingsCount} reviews</p>}
           {categories && <ul className="book__categories">{categories.map((category, index) =>
             <p key={index} className="book__category">{category}</p>)}</ul>}
         </div>
