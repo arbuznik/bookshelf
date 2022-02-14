936028cea36006793904ce53fb0110f623e18099
Arbuznik <arbyzov.nikita@yandex.ru>
2022-02-14 23:33:09 +0300

    [fix] fix CRA warnings

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -22,7 +22,7 @@ function SearchForm() {
     if (currentUrl.pathname !== '/') {
       setQuery(currentQuery)
     }
-  }, [currentUrl])
+  }, [currentUrl, currentQuery])
 
   const handleSubmit = evt => {
     evt.preventDefault()
